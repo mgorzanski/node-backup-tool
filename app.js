00beef9ccb835700827f34b4abd8ae7ffb47686b
Mateusz <gorzanski.mateusz@gmail.com>
2018-08-02 15:11:35 +0200

    move function declarations to the top

--- app.js
+++ app.js
@@ -25,30 +25,7 @@ const {
   useWindowsPathSymbols
 } = config;
 
-let filesIndex = []; //main tree of files and catalogs
-const allFolders = []; //all folders in main tree
-
-console.log(`Starting to build index...`);
-
-const progressBar = new _cliProgress.Bar(
-  {
-    format: "Progress [{bar}] {percentage}% | ETA: {eta}s | {value}/{total}"
-  },
-  _cliProgress.Presets.shades_classic
-);
-progressBar.start(dirsToMakeCopyOf.length, 0);
-
-dirsToMakeCopyOf.forEach(dir => {
-  if (allFolders.indexOf(dir) === -1) {
-    //check for folder duplicates
-    const temp = makeTree(dir);
-    filesIndex = filesIndex.concat(temp);
-  }
-  progressBar.increment();
-});
-
-progressBar.stop();
-
+//functions here
 function makeTree(dir) {
   let tree = [];
   let listing;
@@ -81,29 +58,6 @@ function makeTree(dir) {
   return tree;
 }
 
-console.log(`Saving index to filesIndex.json...`);
-
-try {
-  fs.writeFileSync("filesIndex.json", JSON.stringify(filesIndex, null, 2)); //save to json file using 2 space indentation
-} catch (error) {
-  console.error(
-    `\x1b[31m`,
-    `Error. Cannot write changes to filesIndex.json...`,
-    `\x1b[0m`
-  );
-  process.exit();
-}
-
-console.log(
-  `\x1b[32m`,
-  `Finished building index. The index contains ${filesIndex.length} files.`,
-  `\x1b[0m`
-);
-
-if (process.argv[2] === "--build-index-only") process.exit();
-
-console.log(`Starting to make a backup...`);
-
 function generateBackupName() {
   //generate backup date
   const today = new Date();
@@ -131,6 +85,63 @@ function generateBackupName() {
   return backupName;
 }
 
+function mkdirParentSync(dirPath, mode) {
+  try {
+    fs.mkdirSync(dirPath, mode);
+  } catch (error) {
+    mkdirParentSync(path.dirname(dirPath), mode); //every time function is called the folder is one upper (path.dirname removes last directory)
+    mkdirParentSync(dirPath, mode); //after creating the most upper folder, function is called again and now mkdirSync is doing a job
+  }
+}
+//end functions
+
+let filesIndex = []; //main tree of files and catalogs
+const allFolders = []; //all folders in main tree
+
+console.log(`Starting to build index...`);
+
+const progressBar = new _cliProgress.Bar(
+  {
+    format: "Progress [{bar}] {percentage}% | ETA: {eta}s | {value}/{total}"
+  },
+  _cliProgress.Presets.shades_classic
+);
+progressBar.start(dirsToMakeCopyOf.length, 0);
+
+dirsToMakeCopyOf.forEach(dir => {
+  if (allFolders.indexOf(dir) === -1) {
+    //check for folder duplicates
+    const temp = makeTree(dir);
+    filesIndex = filesIndex.concat(temp);
+  }
+  progressBar.increment();
+});
+
+progressBar.stop();
+
+console.log(`Saving index to filesIndex.json...`);
+
+try {
+  fs.writeFileSync("filesIndex.json", JSON.stringify(filesIndex, null, 2)); //save to json file using 2 space indentation
+} catch (error) {
+  console.error(
+    `\x1b[31m`,
+    `Error. Cannot write changes to filesIndex.json...`,
+    `\x1b[0m`
+  );
+  process.exit();
+}
+
+console.log(
+  `\x1b[32m`,
+  `Finished building index. The index contains ${filesIndex.length} files.`,
+  `\x1b[0m`
+);
+
+if (process.argv[2] === "--build-index-only") process.exit();
+
+console.log(`Starting to make a backup...`);
+
 const backupName = generateBackupName();
 const fullBackupDir = backupDir + "\\" + backupName;
 
@@ -143,15 +154,6 @@ try {
     `Error. Cannot create a backup folder... This might be a permission issue. The process has been terminated.`,
     `\x1b[0m`
   );
-}
-
-function mkdirParentSync(dirPath, mode) {
-  try {
-    fs.mkdirSync(dirPath, mode);
-  } catch (error) {
-    mkdirParentSync(path.dirname(dirPath), mode); //every time function is called the folder is one upper (path.dirname removes last directory)
-    mkdirParentSync(dirPath, mode); //after creating the most upper folder, function is called again and now mkdirSync is doing a job
-  }
 }
 
 //copy files
